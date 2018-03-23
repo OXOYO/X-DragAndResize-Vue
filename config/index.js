3d58ef888cc09c4907c773db28a4b19deb2d2207
OXOYO <zmn2007.hi@163.com>
2018-03-23 09:40:28 +0800

    build for docs.

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/X-DragAndResize-Vue/',
 
     /**
      * Source Maps
